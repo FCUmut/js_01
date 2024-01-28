c048bfa376a80b7f8685b044a9b1f35a3b6bd9cd
Furkan Umut <155393351+FCUmut@users.noreply.github.com>
2024-01-28 22:45:56 +0300

    commit
    commit

---
+++ 02_comments_shortcuts/script.js
@@ -0,0 +1,7 @@
+// you can hold Shit + Up/Down arrows and pick the lines you want
+
+// Shift + Alt + 'A'  commenting with /* */
+// Ctrl + K + C for commenting single line of code with '//'
+// Ctrl + K + U uncommenting the '//'
+
+// Opening terminal shortcut Ctrl + 'é"'
---
+++ 03_variables/script.js
@@ -0,0 +1,32 @@
+// Ways to declare a variable:
+// var, let, const
+// 'var' was the original declaration, but in 2015 update (ES6 - ECMAScript 6), 'let' and 'const' added.
+
+let firstName = "John";
+let lastName = "Doe";
+console.log(firstName, lastName);
+
+// Declare multiple values at once
+let a, b, c;
+
+const d = 10,
+  e = 20,
+  f = 30;
+
+// Multi-Word Formating:
+
+// firstName = camelCase
+
+// first_name = snake_case
+
+// FirstName = PascalCase
+
+// first-name = kebab-case
+
+// firstname = flatcase , lowercase
+
+// FIRSTNAME = UPPERFLATCASE , UPPERCASE
+
+// FIRST_NAME = SCREAMING_SNAKE_CASE
+
+// First-Name = Train-Case
