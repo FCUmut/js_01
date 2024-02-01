d70f47cb8933c80361e9d5748c3793d3742260b6
Furkan Umut <155393351+FCUmut@users.noreply.github.com>
2024-02-01 20:31:45 +0300

    commit
    commit

---
+++ 10_capitalize_challenge/script.js
@@ -0,0 +1,16 @@
+const myString = "developer";
+// developer
+// 012345678
+//-987654321
+
+let solutionOne;
+let solutionTwo;
+let solutionThree;
+
+solutionOne = myString.charAt(0).toUpperCase() + myString.substring(1);
+solutionTwo = myString[0].toUpperCase() + myString.slice(-8, 9);
+solutionThree = `${myString[0].toUpperCase()}${myString.slice(1)}`;
+
+console.log(solutionOne);
+console.log(solutionTwo);
+console.log(solutionThree);
